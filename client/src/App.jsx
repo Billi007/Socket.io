abca1a397ae0138eb74bd60af19761228d97f31c
Tanishka <guptatanishka2008@gmail.com>
2024-11-08 18:07:47 +0530

    Add files

---
+++ client/src/App.jsx
@@ -0,0 +1,50 @@
+import { useState, useEffect } from 'react'
+import {io} from 'socket.io-client'
+import {nanoid} from 'nanoid'
+import './App.css'
+
+const socket = io.connect("http://localhost:8000");    //better approach with key connection
+
+function App() {
+  const [message, setMessage] = useState("");
+  const [chat, setChat] = useState([]);
+  
+  const sendChat = (e) => {
+    e.preventDefault();
+    socket.emit("chat", {message})
+    setMessage("");
+  }
+
+  useEffect(() => {
+    socket.on("chat", (data) => {
+      setChat([...chat, data]);
+    })
+  })
+
+  return (
+    <>
+     <div className="app">
+      <header className="app-header">
+        <h1>Chatty App</h1>
+      </header>
+
+      {chat.map((data ,id) => (
+       <p key={id}>{data.message}</p>
+      ))}
+
+      <form onSubmit={sendChat}>
+        <input 
+        type="text" 
+        name="chat" 
+        placeholder='send message' 
+        value={message}
+        onChange={e => setMessage(e.target.value)}
+        />
+        <button type='submit'>Send</button>
+      </form>
+     </div>
+    </>
+  )
+}
+
+export default App
---
+++ server/app.js
@@ -0,0 +1,33 @@
+import express from 'express';
+import {createServer} from 'http'
+import {Server} from 'socket.io'
+import cors from 'cors'
+
+const app = express();
+const port = 8000;
+const server = createServer(app);
+const io = new Server(server,{
+    cors:{
+        origin: "http://localhost:5173",
+        credentials: true,
+    }
+});
+
+app.use(
+    cors({
+    origin: "http://localhost:5173",
+    credentials: true, 
+ }));
+
+io.on("connection", (socket) => {
+    console.log("User connected");
+
+    socket.on("chat", (data) => {
+        io.emit("chat", "Hello from the server");
+        console.log("Data", data);
+    })
+})
+
+server.listen(port, () => {
+ console.log('listening on port', port);
+})
